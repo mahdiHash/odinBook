79196d442e519003e90ebf53271600d6b1280ab2
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-12 19:54:58 +0430

    chore: remove fields required key
    Since the profile returned by Google OAuth only contains an "id"
    key, there were errors upon creating new user document in the db.
    Only the username is required now and it's set to the "id".

--- models/users.js
+++ models/users.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  first_name: { type: String },
+  last_name: { type: String },
   username: { type: String, required: true, length: { min: 3 } },
-  password: { type: String, required: true, length: { min: 8 }},
+  password: { type: String },
   date_of_birth: { type: Date },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  location: { type: String, required: true },
+  location: { type: String },
   profile_pic_url: { type: String },
   rooms: [{ type: Schema.Types.ObjectId }],
   privates: [{ type: Schema.Types.ObjectId }],

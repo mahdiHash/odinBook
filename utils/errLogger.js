f34dfdb11de43b0a561266690fee9d098eea07d7
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-12 16:15:24 +0430

    handle uncaught errors

--- utils/errLogger.js
+++ utils/errLogger.js
@@ -1,10 +1,18 @@
 const {
   ServerSideErr,
   BadGatewayErr,
+  BadRequestErr,
+  UnauthorizedErr,
+  ForbiddenErr,
+  NotFoundErr,
 } = require('./errors');
 
 const logger = (err, req, res, next) => {
-  if (err instanceof ServerSideErr || err instanceof BadGatewayErr) {
+  if (
+    err instanceof ServerSideErr || err instanceof BadGatewayErr ||
+    !(err instanceof BadRequestErr || err instanceof UnauthorizedErr ||
+      err instanceof ForbiddenErr, err instanceof NotFoundErr)
+  ) {
     console.log('\x1b[31m%s\x1b[0m', err.name);
     console.log('at: ', req.url);
     console.error(err);

aefe9d51e0aa071620080c347587f7df686f28d8
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-12 15:03:13 +0430

    feat: error logger for non-client errors

---
+++ utils/errorLogger.js
@@ -0,0 +1,15 @@
+const {
+  ServerSideErr,
+  BadGatewayErr,
+} = require('./errors');
+
+const logger = (err, req, res, next) => {
+  if (err instanceof ServerSideErr || err instanceof BadGatewayErr) {
+    console.log('\x1b[31m%s\x1b[0m', err.name);
+    console.error(err);
+    console.log('Request Body: ', req.body);
+    console.log('Request parameters: ', req.params);
+  }
+  
+  next(err);
+}

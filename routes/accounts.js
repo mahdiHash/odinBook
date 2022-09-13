140483474ceda07e69654fb2f6ffcee32206e5cf
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-13 17:35:30 +0430

    feat: signup with username and password

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,9 +2,12 @@ const Router = require('express').Router;
 const router = new Router();
 const googleLogin = require('../controllers/accounts/googleLogin');
 const passport = require('../config/passport-google');
+const signupController = require('../controllers/accounts/signup');
 
 router.get('/oauth2/google', passport.authenticate('google'));
 
 router.get('/oauth2/redirect/google', googleLogin);
+
+router.post('/signup', signupController);
 
 module.exports = router;

43eac27ed28f0019fd04f2878ba5feba4527fca7
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-13 16:35:07 +0430

    fix a bug, add another field to servser-side error logger

--- utils/errors/errLogger.js
+++ utils/errors/errLogger.js
@@ -11,11 +11,12 @@ const logger = (err, req, res, next) => {
   if (
     err instanceof ServerSideErr || err instanceof BadGatewayErr ||
     !(err instanceof BadRequestErr || err instanceof UnauthorizedErr ||
-      err instanceof ForbiddenErr, err instanceof NotFoundErr)
+      err instanceof ForbiddenErr || err instanceof NotFoundErr)
   ) {
     console.log('\x1b[31m%s\x1b[0m', err.name);
     console.log('at: ', req.url);
     console.error(err);
+    console.log('Error Description: ', err.description ?? '');
     console.log('Request Body: ', req.body);
     console.log('Request Parameters: ', req.params);
     console.log('Request Query: ', req.query);

3a21ca4ad54e5b1d33b12dab7fd4fd939694a3ca
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-13 15:07:28 +0430

    refactor: change a variable name to a descriptive one

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router;
 const router = new Router();
-const controller = require('../controllers/accounts/googleLogin');
+const googleLogin = require('../controllers/accounts/googleLogin');
 const passport = require('../config/passport-google');
 
 router.get('/oauth2/google', passport.authenticate('google'));
 
-router.get('/oauth2/redirect/google', controller);
+router.get('/oauth2/redirect/google', googleLogin);
 
 module.exports = router;

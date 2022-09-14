c2f5be0db51b22047fb4ea72284b90f97c6745d8
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-14 22:20:07 +0430

    feat: store post._id in each hashtag posts field

--- controllers/posts/createpost.js
+++ controllers/posts/createpost.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/posts');
+const HashTag = require('../../models/hashtags');
 const hashtagExtracter = require('../../utils/hashtagExtracter');
 const { body, validationResult } = require('express-validator');
 const { BadRequestErr } = require('../../utils/errors/errors');
@@ -7,13 +8,14 @@ const controller = [
   // input validation
   body('content', 'Content can\'t be empty').trim().isLength({ min: 1 }).escape(),
 
-  (req, res, next) => {
+  async (req, res, next) => {
     let error = validationResult(req).array();
 
     if (error.length) {
       return next(new BadRequestErr(error[0].msg));
     }
 
+    let hashtags = hashtagExtracter(req.body.content);
     let post = new Post({
       author: req.user.username,
       date: new Date(),
@@ -21,8 +23,26 @@ const controller = [
       is_edited: false,
       comments: [],
       likes: [],
-      hashtags: hashtagExtracter(req.body.content),
+      hashtags: hashtags,
     });
+
+    // store the post._id in the hashtag document in db
+    for (let tag of hashtags) {
+      let existingTag = await HashTag.findOne({ tag }).catch(next);
+      
+      if (existingTag) {
+        existingTag.posts.push(post._id);
+        existingTag.save();
+        continue;
+      }
+
+      let newTag = new HashTag({
+        tag,
+        posts: [post._id],
+      });
+
+      newTag.save();
+    }
 
     post.save();
     res.json(post);

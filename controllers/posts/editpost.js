bfe2e82eab03fa02e99d6527e8d9a89e822622e1
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-15 20:57:00 +0430

    feat: edit posts

---
+++ controllers/posts/editpost.js
@@ -0,0 +1,79 @@
+const Post = require('../../models/posts');
+const Hashtag = require('../../models/hashtags');
+const hashtagExtracter = require('../../utils/hashtagExtracter');
+const { body, validationResult } = require('express-validator');
+const { BadRequestErr, ForbiddenErr } = require('../../utils/errors/errors');
+
+const controller = [
+  // check if the author of the post is the same as user
+  async (req, res, next) => {
+    let post = await Post.findById(req.query.id).catch(next);
+
+    if (!post) {
+      return next(new BadRequestErr('No such post'));
+    }
+
+    if (post.author !== req.user.username) {
+      return next(new ForbiddenErr());
+    }
+    else {
+      res.locals.post = post;
+      next();
+    }
+  },
+
+  // validation
+  body('content', 'Content can\'t be empty').trim().isLength({ min: 1 }).escape(),
+
+  async (req, res, next) => {
+    let error = validationResult(req).array();
+
+    if (error.length) {
+      return next(new BadRequestErr(error[0].msg));
+    }
+
+    let post = res.locals.post,
+      upPostHashtags = hashtagExtracter(req.body.content);
+
+    // remove the post._id from the old hashtags posts field
+    // if the new set of hashtags doesn't include any of the old ones,
+    // the old hashtag is deleted
+    for (let i = 0; i < post.hashtags.length; i++) {
+      if (!upPostHashtags.includes(post.hashtags[i])) {
+        let tag = await Hashtag.findOne({ tag: post.hashtags[i] }).catch(next);
+        tag.posts.splice(tag.posts.indexOf(post._id), 1);
+        await tag.save();
+      }
+    }
+    
+    // insert the post._id to new hashtags posts field
+    // if the new set of hashtags includes a hashtag which is not in the old set,
+    // the hashtag is new and should be inserted
+    for (let i = 0; i < upPostHashtags.length; i++) {
+      if (!post.hashtags.includes(upPostHashtags[i])) {
+        let tag = await Hashtag.findOne({ tag: upPostHashtags[i] }).catch(next);
+        
+        // the hashtag is totally new, so create a new document
+        if (!tag) {
+          let newTag = new Hashtag({
+            tag: upPostHashtags[i],
+            posts: [post._id],
+          });
+          
+          await newTag.save();
+        }
+        else {
+          tag.posts.push(post._id);
+          await tag.save();
+        }
+      }
+    }
+    
+    post.content = req.body.content;
+    post.hashtags = upPostHashtags;    
+    post.save();
+    res.json(post);
+  }
+];
+
+module.exports = controller;
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const createController = require('../controllers/posts/createpost');
+const editController = require('../controllers/posts/editpost');
 
 router.post('/create/', createController);
+
+router.post('/edit/', editController);
 
 module.exports = router;

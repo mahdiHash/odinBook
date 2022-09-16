2e1c7bbd08039c8ca5d6da53eb0285ec31c1c57f
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-16 11:12:45 +0430

    feat: delete post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const createController = require('../controllers/posts/createpost');
 const editController = require('../controllers/posts/editpost');
+const deleteController = require('../controllers/posts/deletepost');
 
 router.post('/create/', createController);
 
 router.post('/edit/', editController);
+
+router.delete('/delete/', deleteController);
 
 module.exports = router;

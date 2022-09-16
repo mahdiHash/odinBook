199af8b7e4f6aca02c210f33973321f64851bdda
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-16 19:37:55 +0430

    feat: accept friend request

---
+++ controllers/friend-req/acceptReq.js
@@ -0,0 +1,29 @@
+const Friend_req = require('../../models/friend_req');
+const User = require('../../models/users');
+const { BadRequestErr, ForbiddenErr } = require('../../utils/errors/errors');
+
+const controller = async (req, res, next) => {
+  let request = await Friend_req.findById(req.query.id).catch(next);
+
+  if (!request) {
+    return next(new BadRequestErr('No such request'));
+  }
+
+  if (request.to.toString() !== req.user._id.toString()) {
+    return next(new ForbiddenErr());
+  }
+
+  let targetUser = await User.findById(request.to).catch(next);
+  targetUser.friends.push(request.from);
+  targetUser.save();
+
+  let senderUser = await User.findById(request.from).catch(next);
+  senderUser.friends.push(request.to);
+  senderUser.save();
+
+  request.remove();
+
+  res.end();
+}
+
+module.exports = controller;
--- routes/friend-req.js
+++ routes/friend-req.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const sendReqController = require('../controllers/friend-req/sendReq');
+const acceptReqController = require('../controllers/friend-req/acceptReq');
 
 router.post('/send', sendReqController);
+
+router.post('/accept', acceptReqController);
 
 module.exports = router;

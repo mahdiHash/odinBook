184fe9e8e8ffa2952b945b4f323b0f4b06562ad6
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-18 17:09:23 +0430

    feat: get all friends requests

--- routes/friend-req.js
+++ routes/friend-req.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const sendReqController = require('../controllers/friend-req/sendReq');
 const acceptReqController = require('../controllers/friend-req/acceptReq');
+const getAllReqsController = require('../controllers/friend-req/getReqs');
 
 router.post('/send', sendReqController);
 
 router.post('/accept', acceptReqController);
+
+router.get('/', getAllReqsController);
 
 module.exports = router;

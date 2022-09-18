5a87b1ec9f27b29386ce7a2de7508b881a373ccf
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-18 16:28:15 +0430

    feat: get user's friends list

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const setUsernameController = require('../controllers/users/setUsername');
+const getFriendsController = require('../controllers/users/getFriends');
 
 router.put('/setusername', setUsernameController);
+
+router.get('/friends', getFriendsController)
 
 module.exports = router;

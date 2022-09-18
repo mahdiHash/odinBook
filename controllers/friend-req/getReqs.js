85ff8c429fb0460f8d6592afd042f1ae715595ae
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-18 17:09:23 +0430

    feat: get all friends requests

---
+++ controllers/friend-req/getReqs.js
@@ -0,0 +1,14 @@
+const Friend_req = require('../../models/friend_req');
+
+const controller = async (req, res, next) => {
+  let { page = 1 } = req.query;
+  let requests = await Friend_req.find({ to: req.user._id })
+    .skip((page - 1) * 10)
+    .limit(10)
+    .populate('from', 'username profile_pic_url')
+    .catch(next);
+  
+  res.json(requests);
+}
+
+module.exports = controller;

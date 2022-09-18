e2939fabb8e4736ef0fe2f993141bde9c4116b14
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-18 17:25:23 +0430

    feat: deny friend request

--- routes/friend-req.js
+++ routes/friend-req.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 const sendReqController = require('../controllers/friend-req/sendReq');
 const acceptReqController = require('../controllers/friend-req/acceptReq');
 const getAllReqsController = require('../controllers/friend-req/getReqs');
+const denyReqController = require('../controllers/friend-req/denyReq');
 
 router.post('/send', sendReqController);
 
 router.post('/accept', acceptReqController);
+
+router.delete('/deny', denyReqController);
 
 router.get('/', getAllReqsController);
 

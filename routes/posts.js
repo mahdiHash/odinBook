5419f6478a2db644a8210056c781740ef0989649
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-19 21:51:19 +0430

    feat: get posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const createController = require('../controllers/posts/createpost');
 const editController = require('../controllers/posts/editpost');
 const deleteController = require('../controllers/posts/deletepost');
+const getPostsController = require('../controllers/posts/getPosts');
 
 router.post('/create/', createController);
 
 router.post('/edit/', editController);
 
 router.delete('/delete/', deleteController);
+
+router.get('/', getPostsController);
 
 module.exports = router;

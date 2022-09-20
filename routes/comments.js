6fea9b8da3ed78eac90d313fe863705d4022801b
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-20 19:54:59 +0430

    feat: edit comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const createCommentController = require('../controllers/comments/createComment');
+const editCommentController = require('../controllers/comments/editComment');
 
 router.post('/create', createCommentController);
+
+router.put('/edit', editCommentController);
 
 module.exports = router;

977a8dab4bf18d6cb836b80c6b071afcaf0eaf7a
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-20 19:32:21 +0430

    feat: get posts comments

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,15 @@ const createController = require('../controllers/posts/createpost');
 const editController = require('../controllers/posts/editpost');
 const deleteController = require('../controllers/posts/deletepost');
 const getPostsController = require('../controllers/posts/getPosts');
+const getPostCommentsController = require('../controllers/posts/getComments');
 
 router.post('/create/', createController);
 
 router.post('/edit/', editController);
 
 router.delete('/delete/', deleteController);
+
+router.get('/comments', getPostCommentsController);
 
 router.get('/', getPostsController);
 

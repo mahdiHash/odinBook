d3c40658b2edc509a4ffa32aca832ed02926b706
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-23 12:51:09 +0330

    refactor query and add pagination to get comments of posts

--- controllers/posts/getComments.js
+++ controllers/posts/getComments.js
@@ -1,26 +1,24 @@
 const Post = require('../../models/posts');
-const Comment = require('../../models/comments');
 const { NotFoundErr } = require('../../utils/errors/errors');
 
 const controller = async (req, res, next) => {
   let post = await Post.findById(req.query.postid)
     .select('comments')
+    .populate({
+      path: 'comments',
+      match: { in_reply_to: null }, // don't get replies
+      populate: { path: 'author', select: 'username profile_pic_url' }
+    })
     .catch(next);
-  let comments = [];
 
   if (!post) {
     return next(new NotFoundErr('No such post'));
   }
 
-  // populate every comment of the post
-  for (let commentId of post.comments) {
-    let comment = await Comment.findById(commentId)
-      .populate('author', 'username profile_pic_url')
-      .catch(next);
-    comments.push(comment);
-  }
+  let { page = 1 } = req.query;
+  let skip = (page - 1) * 20;
 
-  res.json(comments);
+  res.json(post.comments.slice(skip, skip + 20));
 }
 
 module.exports = controller;

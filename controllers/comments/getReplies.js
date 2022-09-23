6fc63d80ae8eb91004a758f77bbedbf442185502
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-23 14:30:30 +0330

    refactor: query for getting replies

--- controllers/comments/getReplies.js
+++ controllers/comments/getReplies.js
@@ -4,23 +4,17 @@ const { NotFoundErr } = require('../../utils/errors/errors');
 const controller = async (req, res, next) => {
   let comment = await Comment.findById(req.query.commentid)
     .select('replies')
+    .populate({
+      path: 'replies',
+      populate: { path: 'author', select: 'username profile_pic_url' }
+    })
     .catch(next);
-  let replies = [];
 
   if (!comment) {
     return next(new NotFoundErr('No such comment'));
   }
 
-  // populate each reply
-  for (let replyId of comment.replies) {
-    let reply = await Comment.findById(replyId)
-      .populate('author', 'username profile_pic_url')
-      .catch(next);
-
-    replies.push(reply);
-  }
-
-  res.json(replies);
+  res.json(comment.replies);
 }
 
 module.exports = controller;

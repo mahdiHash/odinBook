f9239fc4f3c986d1b2eab54b296a6498f59ce8c7
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-23 15:32:28 +0330

    feat: get user profile

---
+++ controllers/users/getUser.js
@@ -0,0 +1,39 @@
+const User = require('../../models/users');
+const { NotFoundErr } = require('../../utils/errors/errors');
+
+const controller = async (req, res, next) => {
+  if (req.user.username === req.params.username) {
+    res.locals.targetUser = await User.findOne({ username: req.params.username })
+      .select('-password -friends -posts')
+      .populate({
+        path: 'privates',
+        select: 'members',
+        populate: {
+          path: 'members',
+          select: 'username profile_pic_url',
+        },
+      })
+      .populate({
+        path: 'rooms',
+        select: 'members profile_pic_url',
+        populate: {
+          path: 'members',
+          select: 'username profile_pic_url',
+        },
+      })
+      .catch(next);
+  }
+  else {
+    res.locals.targetUser = await User.findOne({ username: req.params.username })
+      .select('-password -privates -rooms -friends -posts')
+      .catch(next);
+  }
+
+  if (!res.locals.targetUser) {
+    return next(new NotFoundErr('User not found'));
+  }
+
+  res.json(res.locals.targetUser);
+}
+
+module.exports = controller;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const setUsernameController = require('../controllers/users/setUsername');
 const getFriendsController = require('../controllers/users/getFriends');
 const getPostsController = require('../controllers/users/getPosts');
+const getUserProfileController = require('../controllers/users/getUser');
 
 router.put('/setusername', setUsernameController);
 
 router.get('/friends', getFriendsController);
 
 router.get('/:username/posts', getPostsController);
+
+router.get('/:username', getUserProfileController);
 
 module.exports = router;

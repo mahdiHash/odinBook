d1d1476bb75bb2121ea8843f3a4d89b55adbc5c8
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-24 08:37:37 +0330

    feat: set user's profile info

---
+++ controllers/users/setProfileInfo.js
@@ -0,0 +1,37 @@
+const User = require('../../models/users');
+const { body, validationResult } = require('express-validator');
+const { BadRequestErr } = require('../../utils/errors/errors');
+
+const controller = [
+  // validation
+  body('first_name', 'First name can\'t be empty and has to have at least 1 and only alphabetical character.')
+    .trim().notEmpty().isAlpha(),
+  body('last_name', 'First name can\'t be empty and has to have at least 1 and only alphabetical character.')
+    .trim().notEmpty().isAlpha(),
+  body('date_of_birth', 'Date of birth is not valid').isDate(),
+  body('location', 'Location should be alphabetical with 2 characters').trim().isAlpha().isLength({ min: 2, max: 2 }),
+
+  async (req, res, next) => {
+    let inputErrors = validationResult(req).array();
+
+    if (inputErrors.length) {
+      let message = inputErrors.map((err) => err.msg);
+
+      return next(new BadRequestErr(message));
+    }
+
+    let user = await User.findById(req.user._id)
+      .select('-password')
+      .catch(next);
+
+    user.location = req.body.location;
+    user.date_of_birth = new Date(req.body.date_of_birth);
+    user.first_name = req.body.first_name;
+    user.last_name = req.body.last_name;
+
+    user.save();
+    res.json(user);
+  }
+];
+
+module.exports = controller;
--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,13 @@ const setUsernameController = require('../controllers/users/setUsername');
 const getFriendsController = require('../controllers/users/getFriends');
 const getPostsController = require('../controllers/users/getPosts');
 const getUserProfileController = require('../controllers/users/getUser');
+const setProfileInfoController = require('../controllers/users/setProfileInfo');
 
 router.put('/setusername', setUsernameController);
 
 router.get('/friends', getFriendsController);
+
+router.post('/setprofileinfo', setProfileInfoController);
 
 router.get('/:username/posts', getPostsController);
 

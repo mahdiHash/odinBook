1a9584b0574cbccf09f9a4f8586ce2ac4dc570e0
mahdiHash <mahdihashemiii939@gmail.com>
2022-09-30 15:28:48 +0330

    chore: remove online members field

--- models/rooms.js
+++ models/rooms.js
@@ -4,7 +4,6 @@ const Room = new Schema({
   members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   messages: [{ type: Object }],
   profile_pic_url: { type: String },
-  online_members: [{ type: Schema.Types.ObjectId }],
 });
 
 module.exports = mongoose.model('Room', Room);

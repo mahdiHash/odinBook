6458fac3c85bb573d7c5804b115bdf82714ad2e3
mahdiHash <mahdihashemiii939@gmail.com>
2022-10-03 22:52:04 +0330

    chore: sort the posts from old to new

--- controllers/posts/getPosts.js
+++ controllers/posts/getPosts.js
@@ -4,7 +4,7 @@ const controller = async (req, res, next) => {
   let { page = 1 } = req.query; 
   let userFriends = req.user.friends;
   let posts = await Post.find({ author: { $in: userFriends }})
-  .sort('-date')
+  .sort('date')
   .skip((page - 1) * 10)
   .limit(10)
   .populate('author', 'username profile_pic_url')

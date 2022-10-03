42dcab1d23d419448950dd75ae1170364516d8ba
mahdiHash <mahdihashemiii939@gmail.com>
2022-10-03 14:47:23 +0330

    feat: implement socket.io and add connection event handler

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 
 const http = require('http');
+const { Server } = require('socket.io');
+const passport = require('./config/passport-jwt');
 const app = require('./app');
 const port = process.env.PORT || 3000;
+const registerIoHandlers = require('./socket-io/registerIoHandlers');
 
 app.set('port', port);
 
 const server = http.createServer(app);
+const io = new Server(server);
+const wrap = (middleware) => (socket, next) => middleware(socket.request, {}, next);
+
+io.use(wrap(passport.initialize()));
+io.use(wrap(passport.authenticate('jwt', { session: false })));
+
+io.onlineUsersById = {};
+registerIoHandlers(io);
 
 server.listen(port);
 server.on('listening', () => {

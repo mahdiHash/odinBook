bd52d2ed8ed692570775f3841dd9c4eb1decc77e
mahdiHash <mahdihashemiii939@gmail.com>
2022-10-03 13:35:12 +0330

    feat: change profile pic of a room

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const changeNameController = require('../controllers/rooms/changeName');
+const setProfilePicController = require('../controllers/rooms/setProfilePic');
 
 router.put('/setName', changeNameController);
+router.put('/setProfilePic', setProfilePicController);
 
 module.exports = router;

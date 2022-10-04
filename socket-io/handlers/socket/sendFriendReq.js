1caff2a6b624a5ad4313ddee44a6837d69daf20d
mahdiHash <mahdihashemiii939@gmail.com>
2022-10-04 21:38:17 +0330

    chore: typo

--- socket-io/handlers/socket/sendFriendReq.js
+++ socket-io/handlers/socket/sendFriendReq.js
@@ -1,6 +1,6 @@
 const sendFriendReq = async (io, socket, targetUser) => {
   if (io.onlineUsersById[targetUser]) {
-    io.to(io.onlineUsersById[targetUser]).emit('friendReqRecieved', {
+    io.to(io.onlineUsersById[targetUser]).emit('friendReqreceived', {
       _id: socket.user._id,
       username: socket.user.username,
       profile_pic_url: socket.user.profile_pic_url,
